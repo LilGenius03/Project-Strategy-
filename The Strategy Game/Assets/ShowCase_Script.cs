7c7167222f65b174810e608072126a4cf1c074a0
LilGenius03 <finnarcher03@gmail.com>
2024-09-30 00:05:36 +0100

    Models and other changes made

---
+++ The Strategy Game/Assets/ShowCase_Script.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class ShowCase_Script : MonoBehaviour
+{
+    [SerializeField] private Transform[] models; 
+    [SerializeField] private Light[] spotlights;
+    [SerializeField] private float transitionSpeed = 5.0f;
+    [SerializeField] private GameObject Text;
+    private int currentModelIndex = 0; 
+    private bool isControllingCamera = false;
+    [SerializeField] private Vector3 offset = new Vector3(0, 1.5f, -5f);
+
+    private void Start()
+    {
+        Text.SetActive(true);
+        spotlights[0].enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            isControllingCamera = true;
+            Text.SetActive(false);
+        }
+
+        
+        if (isControllingCamera)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                MoveToNextModel();
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                MoveToPreviousModel();
+            }
+
+            
+            Vector3 targetPosition = models[currentModelIndex].position + offset;
+            transform.position = Vector3.Lerp(transform.position, targetPosition, transitionSpeed * Time.deltaTime);
+        }
+
+        UpdateSpotlights();
+    }
+
+   
+    void MoveToNextModel()
+    {
+        currentModelIndex = (currentModelIndex + 1) % models.Length;
+    }
+
+  
+    void MoveToPreviousModel()
+    {
+        currentModelIndex--;
+        if (currentModelIndex < 0)
+        {
+            currentModelIndex = models.Length - 1;
+        }
+    }
+
+   
+    void UpdateSpotlights()
+    {
+        for (int i = 0; i < spotlights.Length; i++)
+        {
+            spotlights[i].enabled = (i == currentModelIndex);
+        }
+    }
+}
+
